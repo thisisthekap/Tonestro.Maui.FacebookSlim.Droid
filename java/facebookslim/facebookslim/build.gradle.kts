d42b49c6be6df3ad3438c87dab92a710fcf36abb
Albilaga <albilaga.pradana@tonestro.com>
2024-10-25 21:12:33 +0700

    Update to 17.0.2

--- java/facebookslim/facebookslim/build.gradle.kts
+++ java/facebookslim/facebookslim/build.gradle.kts
@@ -30,23 +30,15 @@ configurations {
 }
 
 dependencies {
-    implementation("androidx.appcompat:appcompat:1.3.1")
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.7.20")
-    implementation("androidx.annotation:annotation:1.5.0")
-    implementation("androidx.legacy:legacy-support-core-utils:1.0.0")
-    implementation("com.android.installreferrer:installreferrer:1.0")
-    implementation("androidx.core:core-ktx:1.9.0")
-    implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.cardview:cardview:1.0.0")
-    implementation("androidx.browser:browser:1.0.0")
-    implementation("androidx.activity:activity:1.6.0")
-    implementation("androidx.fragment:fragment:1.5.3")
-    implementation("com.google.zxing:core:3.3.3")
-    implementation("com.google.android.material:material:1.4.0")
 
-    implementation("com.facebook.android:facebook-android-sdk:15.1.0")
+    implementation("com.facebook.android:facebook-android-sdk:17.0.2")
 
-    "copyDependencies"("com.facebook.android:facebook-android-sdk:15.1.0")
+    "copyDependencies"("com.facebook.android:facebook-android-sdk:17.0.2")
+}
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(17)
+    }
 }
 
 project.afterEvaluate {
